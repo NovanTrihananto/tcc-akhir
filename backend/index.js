abe93925e0cf9e1b0070f59cd2d2738b305f89b2
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-06-11 15:01:22 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ db.sync()
   });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
